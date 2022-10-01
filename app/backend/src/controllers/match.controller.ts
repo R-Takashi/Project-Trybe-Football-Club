d2a433915a4e60f23c43d825a37c48eb5baff424
Rodrigo Takashi Condo <rodrigo0takashi@gmail.com>
2022-10-01 11:33:41 -0300

    feat: Adiciona ao endpoint GET /matches, pesquisa por partidas em andamento

--- app/backend/src/controllers/match.controller.ts
+++ app/backend/src/controllers/match.controller.ts
@@ -6,6 +6,14 @@ export default class MatchController {
   private _matchService = new MatchService();
 
   public async getAll(req: Request, res: Response): Promise<Response> {
+    const { inProgress } = req.query as { inProgress: string };
+
+    if (inProgress) {
+      const filter = JSON.parse(inProgress); // convertendo string para boolean https://stackoverflow.com/questions/263965/how-can-i-convert-a-string-to-boolean-in-javascript
+      const { status, response } = await this._matchService.getByProgress(filter);
+      return res.status(status).json(response);
+    }
+
     const { status, response } = await this._matchService.getAll();
 
     return res.status(status).json(response);
--- app/backend/src/services/match.service.ts
+++ app/backend/src/services/match.service.ts
@@ -5,6 +5,23 @@ import Team from '../database/models/teams.model';
 export default class MatchService implements IMatchService {
   public matchModel = Match;
 
+  public async getByProgress(inProgress: boolean): Promise<IResponseService> {
+    const matches = await this.matchModel.findAll({
+      where: { inProgress },
+      include: [{
+        model: Team,
+        as: 'teamHome',
+        attributes: { exclude: ['id'] },
+      }, {
+        model: Team,
+        as: 'teamAway',
+        attributes: { exclude: ['id'] },
+      }],
+    });
+
+    return { status: 200, response: matches };
+  }
+
   public getAll = async (): Promise<IResponseService> => {
     const matches = await this.matchModel.findAll({
       include: [{
